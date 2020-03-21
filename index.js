a283b11dbeb26ecb6ab62bcbd426395f86a39175
tauan-longaretti <tauanlongaretti@gmail.com>
2020-03-20 21:20:25 -0400

    necessary dependencies installed and server is set up

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require("./server.js");
+
+server.listen(7000, () => {
+    console.log("=== Server listening on port 7000 ===");
+})
